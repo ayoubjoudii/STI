1cab91f2efae92f9e9a5cf358ce9bcb068ad4dfe
ayoub <ayoub.joudi556@gmail.com>
2024-05-17 22:58:27 +0100

    new file:   2019/controle.js 	new file:   2019/css.css 	new file:   2019/sondage.html 	new file:   2019/sondage.php 	new file:   2019/statistiques.html 	new file:   2019/statistiques.php

---
+++ 2019/controle.js
@@ -0,0 +1,88 @@
+function valid(ch){
+    let p = ch.indexOf('@')
+    let ch1 = ch.substr(0,p)
+    let p1 = ch.indexOf('.')
+    let ch2 = ch.substr(p+1,p1-p)
+    let ch3 = ch.substr(p1+1)
+    let test1 = ch1.length>=2
+    let test2 = ch2.length>=2
+    let test3 = ch3.length>=2 && ch3.length<=4
+
+    for (let index = 0; index < ch1.length; index++) {
+        if (!(("0"<=ch1[index] && ch1[index]<="9") || ("A"<=ch1[index].toUpperCase && ch1[index].toUpperCase<="Z" ))){
+            test1 = false
+        }    
+    }
+
+    for (let index = 0; index < ch2.length; index++) {
+        if (!(("0"<=ch2[index] && ch2[index]<="9") || ("A"<=ch2[index].toUpperCase && ch2[index].toUpperCase<="Z" ))){
+            test2 = false
+        }    
+    }
+    for (let index = 0; index < ch3.length; index++) {
+        if (!("A"<=ch3[index].toUpperCase && ch3[index].toUpperCase<="Z" )){
+            test3 = false
+        }    
+    }
+
+    return test1 && test2 && test3  
+}
+function valid1(mdp){
+    let test = (mdp.length == 6)
+    let nm = 0
+    let nl = 0
+    let nc = 0
+    for (let index = 0; index < mdp.length; index++) {
+        if ("A"<=mdp[index] && mdp[index]<="Z"){
+            nm ++
+        }
+        if ("a"<=mdp[index] && mdp[index]<="z"){
+            nl++
+        }
+        if ("0"<=mdp[index] && mdp[index]<="9"){
+            nc++
+        }
+        
+    }
+    return test && nm >= 1 && nl >= 1 && nc >= 1
+}
+
+function test(){
+    let mail = document.getElementById("mail").value
+    let mdp = document.getElementById("mdp").value
+    let genre  = document.getElementById("sel")
+    let o1  = document.getElementById("1O")
+    let o2  = document.getElementById("2O")
+    let o3  = document.getElementById("3O")
+    let n1  = document.getElementById("1N")
+    let n2  = document.getElementById("2N")
+    let n3  = document.getElementById("3N")
+    let s1  = document.getElementById("1S")
+    let s2  = document.getElementById("2S")
+    let s3  = document.getElementById("3S")
+
+    if (!valid(mail)){
+        alert("verifier email")
+        return false
+    }
+    if (!valid1(mdp)){
+        alert("verifier mdp")
+        return false
+    }
+    if (genre.selectedIndex <= 0){
+        alert("choisi une genre")
+        return false
+    }
+    if (!(o1.checked || n1.checked || s1.checked)){
+        alert("choix q1")
+        return false
+    }
+    if (!(o2.checked || n2.checked || s2.checked)){
+        alert("choix q2")
+        return false
+    }
+    if (!(o3.checked || n3.checked || s3.checked)){
+        alert("choix q3")
+        return false
+    }
+}
---
+++ 2019/sondage.php
@@ -0,0 +1,38 @@
+<?php
+$cnx = mysqli_connect('localhost','root','','2019');
+if (!isset($_POST["mail"]) || !isset($_POST["mdp"]) || !isset($_POST["sel"]) || !isset($_POST["1"]) || !isset($_POST["2"]) ||!isset($_POST["3"]));
+$mail = $_POST["mail"];
+$mdp = $_POST["mdp"];
+$genre = $_POST["sel"];
+$q1 = $_POST["1"];
+$q2 = $_POST["2"];
+$q3 = $_POST["3"];
+
+$resm = mysqli_query($cnx,"select * from participant where mail = '$mail' and mdp <> '$mdp' ");
+if(mysqli_num_rows($resm)!=0){
+    die("erreur d'authentification");
+}
+$res0 = mysqli_query($cnx,"select idparticipant from participant where mail = '$mail' ");
+$id = mysqli_fetch_array($res0)[0];
+$ress = mysqli_query($cnx,"select * from reponse where nums = '1' and idparticipant = '$id' ");
+if (mysqli_num_rows($ress)!=0){
+    $resp = mysqli_query($cnx,"update reponse set rep ='$q1' where idparticipant = '$id' and numq='1'  ");
+    $resp1 = mysqli_query($cnx,"update reponse set rep ='$q2' where idparticipant = '$id' and numq='2'  ");
+    $resp2 = mysqli_query($cnx,"update reponse set rep ='$q3' where idparticipant = '$id' and numq='3'  ");
+    if($resp && $resp1 && $resp2) die("mise a jour succees");
+}
+else{
+    $res = mysqli_query($cnx,"insert into reponse values('1','1','$id','$q1'),('2','1','$id','$q2'),('3','1','$id','$q3') ");
+    if($res) die("insertion success");
+}
+$resm = mysqli_query($cnx,"select * from participant where mail = '$mail' and mdp = '$mdp' ");
+if(mysqli_num_rows($resm)== 0){
+    $resi = mysqli_query($cnx,"insert into participant(mail,mdp,genre) values('$mail','$mdp','$sel') ");
+    $res0 = mysqli_query($cnx,"select idparticipant from participant where mail = '$mail' ");
+    $id = mysqli_fetch_array($res0)[0];
+    $resi1 = mysqli_query($cnx,"insert into reponse values('1','1','$id','$q1'),('2','1','$id','$q2'),('3','1','$id','$q3') ");
+    
+}
+
+
+?>
